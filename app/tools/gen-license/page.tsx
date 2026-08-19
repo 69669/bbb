61e5976d39555bc0f9a04c735b5f66a4b514b0bc
甜甜 <145662294+iosyyds@users.noreply.github.com>
2026-08-19 21:49:13 +0800

    Update page.tsx

--- app/tools/gen-license/page.tsx
+++ app/tools/gen-license/page.tsx
@@ -13,7 +13,7 @@ function simpleHash(str: string): number {
   return Math.abs(hash);
 }
 
-const ADMIN_PASSWORD_HASH = 101217437510939025;
+const ADMIN_PASSWORD_HASH = 535441809;
 
 export default class GeneratePage extends React.Component {
   constructor(props: {}) {
