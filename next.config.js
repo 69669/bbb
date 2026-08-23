d72f93bf776dca355eef4b426ff756d4f7f69416
甜甜 <145662294+iosyyds@users.noreply.github.com>
2026-08-23 22:59:04 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,31 @@ const nextConfig = {
   output: "export",
   basePath: "",
   trailingSlash: true,
-  images: { unoptimized: true },
+  compress: true,
+  poweredByHeader: false,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|jpeg|png|gif|webp|ico)",
+        locale: false,
+        headers: [
+          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+        ],
+      },
+      {
+        source: "/:all*(js|css)",
+        locale: false,
+        headers: [
+          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+        ],
+      },
+    ];
+  },
 };
-
 module.exports = nextConfig;
